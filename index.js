e0368383516c54b8f144c055446c408c762b9379
yeasin-islam <info.yeasinislam@gmail.com>
2025-06-12 18:12:58 +0600

    Use delete mathon for delate single marathon from my marathon page

--- index.js
+++ index.js
@@ -64,6 +64,13 @@ async function run() {
       }
     });
 
+    app.delete("/marathons/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await marathonsCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
