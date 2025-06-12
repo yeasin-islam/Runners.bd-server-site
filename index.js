a9fb7acc208f843185f3527f74ef58157ecceb26
yeasin-islam <info.yeasinislam@gmail.com>
2025-06-12 10:44:18 +0600

    Post data in server

--- index.js
+++ index.js
@@ -34,7 +34,6 @@ async function run() {
       res.send(result);
     });
 
-
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
