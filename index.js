83def669fc19dc3a89563ebbc0cda5987cbed373
yeasin-islam <info.yeasinislam@gmail.com>
2025-06-12 13:53:59 +0600

    Use get mathod for get single marathon in details page

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 3000;
 require("dotenv").config();
@@ -37,6 +37,13 @@ async function run() {
     app.get("/marathons", async (req, res) => {
       const cursor = marathonsCollection.find();
       const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    app.get("/marathons/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await marathonsCollection.findOne(query);
       res.send(result);
     })
 
